95d0be719c5490313a2e198e9c88871d75706292
mstjepan28 <markovcics@gmail.com>
2025-05-11 17:44:32 +0200

    added main screen

--- src/router/MainRouter.tsx
+++ src/router/MainRouter.tsx
@@ -1,14 +1,11 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { MapScreen } from "../screens/MapScreen";
 
 export const MainRouter = () => {
   const router = createBrowserRouter([
     {
       path: "/",
-      element: (
-        <div className="w-full h-dvh flex items-center justify-center">
-          <span className="text-4xl">Hello world</span>
-        </div>
-      ),
+      element: <MapScreen />,
     },
   ]);
 

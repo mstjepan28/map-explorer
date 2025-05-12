bc158222e0400c0bde6172323f8c56ca1186d7cf
mstjepan28 <markovcics@gmail.com>
2025-05-12 06:55:27 +0200

    show cur position with a red dot

--- src/components/MapElement.tsx
+++ src/components/MapElement.tsx
@@ -4,6 +4,8 @@ import type { LatLngExpression } from "leaflet";
 import "leaflet/dist/leaflet.css";
 
 export const MapElement = ({ center, lineCoords }: { center: LatLngExpression; lineCoords: LatLngExpression[] }) => {
+  const curPosition = lineCoords[lineCoords.length - 1];
+
   return (
     <MapContainer center={center} zoom={20} keyboard={false} scrollWheelZoom>
       <TileLayer
@@ -11,7 +13,11 @@ export const MapElement = ({ center, lineCoords }: { center: LatLngExpression; l
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
 
-      {center && <Polyline pathOptions={{ color: "blue", weight: 10 }} positions={[center, center]} />}
+      <div className="z-10">
+        {center && <Polyline pathOptions={{ color: "blue", weight: 10 }} positions={[center, center]} />}
+        {curPosition && <Polyline pathOptions={{ color: "red", weight: 10 }} positions={[curPosition, curPosition]} />}
+      </div>
+
       {lineCoords && <Polyline pathOptions={{ color: "lime" }} positions={lineCoords} />}
     </MapContainer>
   );
